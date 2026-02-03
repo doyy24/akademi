e35b65ced53b91d42b2fe052248f42fe6cc63d7c
doyy24 <rasyiddo789@gmail.com>
2026-02-03 19:52:18 +0700

    Data

--- siswa/tambah-siswa.php
+++ siswa/tambah-siswa.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Siakad | webDev school</title>
+    <title>X PPLG - webDev school</title>
 
     <!-- Custom fonts for this template-->
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
